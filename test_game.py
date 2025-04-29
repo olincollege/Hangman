18517706e80aa2c7993e72718924c136ed900139
Anika Mahesh <anika.mahesh@outlook.com>
2025-04-29 12:00:48 -0400

    added more tests

--- test_game.py
+++ test_game.py
@@ -19,27 +19,62 @@ def test_take_turn_one_turn_incorrect():
     game.take_turn("g")
     assert game.known_word == ["_","_","_"]
 
-def test_take_turn_one_turn_incorrect():
+def test_take_turn_incorrect_input():
     # tests a case where user takes their first turn and guesses
     # incorrectly.
     game = Game()
     game._secret_word = list("cat")
     game.known_word = ["_","_","_"]
     game.take_turn("g")
-
     assert game.mistakes_made == 1
 
-def test_take_turn_one_turn_game_win():
+def test_take_turn_repeat_input():
+    # tests a case where user takes their first turn and guesses
+    # incorrectly.
+    game = Game()
+    game._secret_word = list("cat")
+    game.known_word = ["_","_","_"]
+    game.take_turn("g")
+    game.take_turn("g")
+    assert game.mistakes_made == 1
+
+def test_take_turn_incorrect_input():
+    # tests a case where user takes their first turn and guesses
+    # incorrect input
+    game = Game()
+    game._secret_word = list("cat")
+    game.known_word = ["_","_","_"]
+    game.take_turn("`")
+    assert game.mistakes_made == 0  
+
+def test_game_win_win():
     # tests a case where user takes their turn and they win
     game = Game()
     game._secret_word = list("cat")
     game.known_word = ["c","a","_"]
-    assert game.take_turn("t") == True
+    game.take_turn("t")
+    assert game.check_win() == True
 
-def test_take_turn_one_turn_game_lose():
-    # tests a case where user takes their turn and they win
+def test_game_win_no_win():
+    # tests a case where user does not win
     game = Game()
     game._secret_word = list("cat")
     game.known_word = ["_","_","_"]
-    game.mistakes_made = 5
-    assert game.take_turn("h") == False
+    assert game.check_win() == False
+
+def test_game_lose_lose():
+    # tests a case where user takes their turn and they lose
+    game = Game()
+    game._secret_word = list("cat")
+    game.known_word = ["_","_","_"]
+    game.mistakes_made = 6
+    assert game.check_loss() == True
+
+
+def test_game_lose_no_lose():
+    # tests a case where user takes their turn and they dont lose
+    game = Game()
+    game._secret_word = list("cat")
+    game.known_word = ["_","_","_"]
+    game.mistakes_made = 1
+    assert game.check_loss() == False
